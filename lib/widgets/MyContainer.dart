31da595d7bf5232612b82d2ed11848e4b5ed69c2
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-24 06:36:16 +0200

    margin

--- lib/widgets/MyContainer.dart
+++ lib/widgets/MyContainer.dart
@@ -4,31 +4,26 @@ class MyContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.centerRight,
+      alignment: Alignment.bottomRight,
       color: Colors.white,
       child: DefaultTextStyle(
         style: TextStyle(),
         // Se utiliza SizedBox cuando solo quieres definir ancho y alto de un contenedor, sin mas parametros
-        child: SizedBox(
-          width: 300,
-          height: 500,
-          child: Center(
-            child: Container(
-              width: 250,
-              height: 250,
-              color: Colors.amber,
-              alignment: Alignment.center,
-              padding: EdgeInsets.only(
-                bottom: 20,
-                right: 20,
-              ),
-              child: Text(
-                'Hola amundio',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black,
-                ),
-              ),
+        child: Container(
+          margin: EdgeInsets.only(bottom: 50, right: 35),
+          width: 250,
+          height: 250,
+          color: Colors.amber,
+          alignment: Alignment.center,
+          padding: EdgeInsets.only(
+            bottom: 20,
+            right: 20,
+          ),
+          child: Text(
+            'Hola amundio',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.black,
             ),
           ),
         ),
