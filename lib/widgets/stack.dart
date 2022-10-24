cd92635cc689bd09fe8f738787d299cb18d5dbb9
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-24 20:06:38 +0200

    MyStack

--- lib/widgets/stack.dart
+++ lib/widgets/stack.dart
@@ -3,27 +3,44 @@ import 'package:flutter/material.dart';
 class MyStack extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DefaultTextStyle(
-      style: TextStyle(color: Colors.white),
-      child: Container(
-        color: Colors.white,
-        child: Stack(
-          alignment: Alignment.topCenter,
-          children: [
-            Container(
-              width: 150,
-              height: 150,
-              color: Colors.green,
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: DefaultTextStyle(
+        style: TextStyle(color: Colors.white),
+        child: Container(
+          color: Colors.white,
+          child: Center(
+            child: Container(
+              color: Colors.grey,
+              width: 500,
+              height: 500,
+              child: Stack(
+                // No es lo mismo tener AlignmentDirectional que Alignment debido a que se comportan teniendo en cuenta la direccion en la que comienzan
+                // a escribir en los paises de escritura arabe por ejemplo de derecha a izq
+                alignment: AlignmentDirectional.topEnd,
+                // alignment: Alignment.topLeft,
+                children: [
+                  Container(
+                    width: 150,
+                    height: 150,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    color: Colors.red,
+                  ),
+                  SizedBox(
+                    width: 100,
+                    child: Text(
+                      "Hola",
+                      textAlign: TextAlign.right,
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Container(
-              width: 50,
-              height: 50,
-              color: Colors.red,
-            ),
-            Text(
-              "Hola",
-            ),
-          ],
+          ),
         ),
       ),
     );
