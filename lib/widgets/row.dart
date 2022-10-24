8236a9e2aeec87c73718a48c1dd8f87f710c925b
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-24 17:42:29 +0200

    Row

--- lib/widgets/row.dart
+++ lib/widgets/row.dart
@@ -6,28 +6,61 @@ class MyRow extends StatelessWidget {
     // TODO: implement build
     return Container(
       color: Colors.grey,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            width: 50,
+            height: 60,
+            color: Colors.red,
+          ),
+          Container(
+            width: 50,
+            height: 50,
+            color: Colors.blue,
+          ),
+          Container(
+            width: 50,
+            height: 100,
+            color: Colors.yellow,
+          ),
+        ],
+      ),
+      /*
       child: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              width: 50,
-              height: 50,
-              color: Colors.red,
-            ),
-            Container(
-              width: 50,
-              height: 50,
-              color: Colors.blue,
-            ),
-            Container(
-              width: 50,
-              height: 50,
-              color: Colors.yellow,
-            ),
-          ],
+        // Si el contenedor que contiene la celda, NO tiene una altura, la altura sera calculada con el elemento mas alto de la celda
+        child: Container(
+          color: Colors.green,
+          // height: 200,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            // Este parametro hace que los elementos dentro de la celda se alineen segun conveniencia
+            // crossAxisAlignment: CrossAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                width: 50,
+                height: 60,
+                color: Colors.red,
+              ),
+              Container(
+                width: 50,
+                height: 50,
+                color: Colors.blue,
+              ),
+              Container(
+                width: 50,
+                height: 100,
+                color: Colors.yellow,
+              ),
+            ],
+          ),
         ),
       ),
+       */
     );
   }
 }
