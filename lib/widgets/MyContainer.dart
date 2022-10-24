5c694a24657729a4ae25cf5078ed07f2837b4e61
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-24 06:40:27 +0200

    margin ignoring width and height

--- lib/widgets/MyContainer.dart
+++ lib/widgets/MyContainer.dart
@@ -4,15 +4,18 @@ class MyContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.bottomRight,
+      alignment: Alignment.center,
       color: Colors.white,
       child: DefaultTextStyle(
         style: TextStyle(),
         // Se utiliza SizedBox cuando solo quieres definir ancho y alto de un contenedor, sin mas parametros
         child: Container(
-          margin: EdgeInsets.only(bottom: 50, right: 35),
-          width: 250,
-          height: 250,
+          margin: EdgeInsets.symmetric(
+            horizontal: 40,
+            vertical: 40
+          ),
+          // width: 250,
+          // height: 250,
           color: Colors.amber,
           alignment: Alignment.center,
           padding: EdgeInsets.only(
