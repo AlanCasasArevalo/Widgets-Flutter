273349e8520115e785d114299003e882c4b1e857
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-24 19:57:55 +0200

    MyStack

--- lib/widgets/stack.dart
+++ lib/widgets/stack.dart
@@ -3,9 +3,29 @@ import 'package:flutter/material.dart';
 class MyStack extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.centerLeft,
-      color: Colors.yellow,
+    return DefaultTextStyle(
+      style: TextStyle(color: Colors.white),
+      child: Container(
+        color: Colors.white,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              width: 150,
+              height: 150,
+              color: Colors.green,
+            ),
+            Container(
+              width: 50,
+              height: 50,
+              color: Colors.red,
+            ),
+            Text(
+              "Hola",
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
