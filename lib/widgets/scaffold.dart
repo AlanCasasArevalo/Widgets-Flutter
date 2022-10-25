dba378991830498e25c56c08923c4a2bb4edb9bc
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-25 10:23:46 +0200

    MyScaffold

--- lib/widgets/scaffold.dart
+++ lib/widgets/scaffold.dart
@@ -9,11 +9,12 @@ class MyScaffold extends StatelessWidget {
         systemOverlayStyle: SystemUiOverlayStyle(
           statusBarBrightness: Brightness.dark
         ),
-        // Color de la barra de navegacion
-        backgroundColor: Colors.grey,
+        // Para poner un color hexadecimal, se debe poner de la siguiente manera Color constructor con los siguientes datos 0xff (hexadecimal) + (ColorEnHesadecimalSinEl#)
+        // quedando de la siguiente manera Color(0xffe6ebf4)
+        backgroundColor: Color(0xff01579b),
         // Estilo del texto de la barra de navegacion
         titleTextStyle: TextStyle(
-          color: Colors.black,
+          color: Colors.white,
           fontSize: 18,
         ),
         // Widget de la barra de navegacion (puede ser un text, un row, un column lo que sea un widget)
