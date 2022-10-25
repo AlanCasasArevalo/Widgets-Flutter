79776f974c344104e2cd9d5e6434c5838addfe78
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-25 17:50:57 +0200

    MyScaffold

--- lib/widgets/scaffold.dart
+++ lib/widgets/scaffold.dart
@@ -5,12 +5,14 @@ class MyScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Este parmetro extendBodyBehindAppBar el valor por defecto es false es para que el cuerpo de la app se expanda hasta ocupar toda la ventana disponible,
-      // lo cual dejaria el container con el Hola Amundio detras de la Appbar, no es que haya desaparecido hola amundio, es que esta detras de la Appbar
-      extendBodyBehindAppBar: true,
+      extendBodyBehindAppBar: false,
       body: Container(
         color: Colors.grey,
-        child: Text("Hola amundio"),
+        width: double.infinity,
+        height: double.infinity,
+        child: Center(
+          child: Text("Hola amundio"),
+        ),
       ),
       appBar: AppBar(
         elevation: 0,
