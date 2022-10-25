e9341f33140482cd85cc73214e834924c8a4bb43
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-25 10:08:52 +0200

    MyScaffold

--- lib/widgets/scaffold.dart
+++ lib/widgets/scaffold.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class MyScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
       appBar: AppBar(
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarBrightness: Brightness.dark
+        ),
         // Color de la barra de navegacion
-        backgroundColor: Colors.redAccent,
+        backgroundColor: Colors.grey,
+        // Estilo del texto de la barra de navegacion
         titleTextStyle: TextStyle(
           color: Colors.black,
           fontSize: 18,
         ),
+        // Widget de la barra de navegacion (puede ser un text, un row, un column lo que sea un widget)
         title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: [
             Text(
               "Flutter App ",
@@ -21,7 +26,6 @@ class MyScaffold extends StatelessWidget {
             Text(
               "Best",
               style: TextStyle(
-                color: Colors.green,
                 fontSize: 20,
                 fontWeight: FontWeight.bold
               ),
