3b54cfa10b39f569ae055f7ab68305bf2b576a2f
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-25 09:39:42 +0200

    MyScaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'widgets/stack.dart';
+import 'widgets/scaffold.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyStack(),
+      home: MyScaffold(),
     );
   }
 }
--- lib/widgets/row.dart
+++ lib/widgets/row.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class MyRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Container(
       color: Colors.grey,
       child: Row(
