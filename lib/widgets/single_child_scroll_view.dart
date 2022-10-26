396df2df21e77458addd0906d598026dabd55fcb
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-26 19:04:43 +0200

    MySingleChildScrollView

--- lib/widgets/single_child_scroll_view.dart
+++ lib/widgets/single_child_scroll_view.dart
@@ -4,15 +4,17 @@ class MySingleChildScrollView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // SingleChildScrollView no puede tener unas dimensiones fijas dichas dimensiones
-      // se las da el padre del SingleChildScrollView
-      body: SingleChildScrollView(
-        child: Column(
-          children: List.generate(
-            18,
-                (index) => Container(
-              height: 100,
-              color: Colors.primaries[index],
+      // Se puede usar un SizedBox si se quiere tener una vista que contenga el SingleChildScrollView con un tamaño determinado
+      body: SizedBox(
+        height: 350,
+        child: SingleChildScrollView(
+          child: Column(
+            children: List.generate(
+              18,
+                  (index) => Container(
+                height: 100,
+                color: Colors.primaries[index],
+              ),
             ),
           ),
         ),
