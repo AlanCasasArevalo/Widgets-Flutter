3e9544a46a58f86b5a74fc2694f35a42d23c8c8a
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-26 20:48:05 +0200

    MyListView

--- lib/widgets/list_view.dart
+++ lib/widgets/list_view.dart
@@ -4,63 +4,65 @@ class MyListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        // Igual que con el componente SingleChildScrollView, este componente tiene el parametro physics: AlwaysScrollableScrollPhysics(), y funciona igual que el otro widget
-        physics: AlwaysScrollableScrollPhysics(),
-        children: [
-          Text("Hola"),
-          Text("Hola"),
-          Text("Hola"),
-          Text("Hola"),
-          Container(
-            height: 100,
-            color: Colors.blue,
-          ),
-          Container(
-            height: 100,
-            color: Colors.red,
-          ),
-          Container(
-            height: 100,
-            color: Colors.blue,
-          ),
-          Text("Hola"),
-          Container(
-            height: 300,
-            color: Colors.red,
-          ),
-          Text("Hola"),
-          Container(
-            height: 300,
-            color: Colors.blue,
-          ),
-          Text("Hola"),
-          Container(
-            height: 300,
-            color: Colors.red,
-          ),
-          Text("Hola"),
-          Container(
-            height: 100,
-            color: Colors.blue,
-          ),
-          Text("Hola"),
-          Container(
-            height: 300,
-            color: Colors.red,
-          ),
-          Text("Hola"),
-          Container(
-            height: 300,
-            color: Colors.blue,
-          ),
-          Text("Hola"),
-          Container(
-            height: 300,
-            color: Colors.red,
-          ),
-          Text("Hola"),
-        ],
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          physics: AlwaysScrollableScrollPhysics(),
+          children: [
+            Text("Hola"),
+            Text("Hola"),
+            Text("Hola"),
+            Text("Hola"),
+            Container(
+              height: 100,
+              color: Colors.blue,
+            ),
+            Container(
+              height: 100,
+              color: Colors.red,
+            ),
+            Container(
+              height: 100,
+              color: Colors.blue,
+            ),
+            Text("Hola"),
+            Container(
+              height: 300,
+              color: Colors.red,
+            ),
+            Text("Hola"),
+            Container(
+              height: 300,
+              color: Colors.blue,
+            ),
+            Text("Hola"),
+            Container(
+              height: 300,
+              color: Colors.red,
+            ),
+            Text("Hola"),
+            Container(
+              height: 100,
+              color: Colors.blue,
+            ),
+            Text("Hola"),
+            Container(
+              height: 300,
+              color: Colors.red,
+            ),
+            Text("Hola"),
+            Container(
+              height: 300,
+              color: Colors.blue,
+            ),
+            Text("Hola"),
+            Container(
+              height: 300,
+              color: Colors.red,
+            ),
+            Text("Hola"),
+          ],
+        ),
       ),
     );
   }
