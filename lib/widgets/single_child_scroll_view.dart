e13a72532544744ba71c2832b58947ae5215f544
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-26 19:02:29 +0200

    MySingleChildScrollView

--- lib/widgets/single_child_scroll_view.dart
+++ lib/widgets/single_child_scroll_view.dart
@@ -4,6 +4,8 @@ class MySingleChildScrollView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // SingleChildScrollView no puede tener unas dimensiones fijas dichas dimensiones
+      // se las da el padre del SingleChildScrollView
       body: SingleChildScrollView(
         child: Column(
           children: List.generate(
