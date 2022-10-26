130423eeda5fe628b260ff08f0d391e9c6c16d16
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-26 18:40:56 +0200

    MySafeArea

--- lib/widgets/save_area.dart
+++ lib/widgets/save_area.dart
@@ -4,6 +4,8 @@ class MySafeArea extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // Al poner el SafeArea como contenedor del container, se ve raro debido a que el SafeArea no hace bien
+      // su trabajo dejando partes en blanco justo por encima del texto Hello
       body: SafeArea(
         child: Container(
           color: Colors.grey,
