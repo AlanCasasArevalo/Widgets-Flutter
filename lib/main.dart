d9298d4bcc4774862fac619c963345bc62676e28
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-26 18:36:24 +0200

    MySafeArea

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'widgets/scaffold.dart';
+import 'widgets/save_area.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyScaffold(),
+      home: MySafeArea(),
     );
   }
 }
