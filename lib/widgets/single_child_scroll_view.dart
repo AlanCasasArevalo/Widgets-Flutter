e6745bf59d684e41cbb3c4d7bd116e48ded7d9e9
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-26 19:17:13 +0200

    MySingleChildScrollView

--- lib/widgets/single_child_scroll_view.dart
+++ lib/widgets/single_child_scroll_view.dart
@@ -4,23 +4,29 @@ class MySingleChildScrollView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(
-            height: 350,
-            child: SingleChildScrollView(
-              child: Column(
-                children: List.generate(
-                  18,
-                      (index) => Container(
-                    height: 100,
-                    color: Colors.primaries[index],
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            Text("Hola"),
+            Expanded(
+              child: SizedBox(
+                height: 350,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: List.generate(
+                      18,
+                          (index) => Container(
+                        height: 100,
+                        color: Colors.primaries[index],
+                      ),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       )
     );
   }
