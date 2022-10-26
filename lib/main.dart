519e54cf8c84c3266ef3798d14739c3426b3d6d4
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-26 18:56:47 +0200

    MySingleChildScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'widgets/save_area.dart';
+import 'widgets/single_child_scroll_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MySafeArea(),
+      home: MySingleChildScrollView(),
     );
   }
 }
---
+++ lib/widgets/single_child_scroll_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MySingleChildScrollView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+    );
+  }
+}
