b7650305ecd9a8dd2600173b7e46c2fd35f05d44
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-26 19:20:43 +0200

    MySingleChildScrollView

--- lib/widgets/single_child_scroll_view.dart
+++ lib/widgets/single_child_scroll_view.dart
@@ -13,12 +13,16 @@ class MySingleChildScrollView extends StatelessWidget {
               child: SizedBox(
                 height: 350,
                 child: SingleChildScrollView(
+                  // Con el parametro reverse (por defecto) hacemos que el scroll empiece al final del listado de elementos (como en whatsapp o telegram)
+                  reverse: false,
                   child: Column(
                     children: List.generate(
                       18,
                           (index) => Container(
                         height: 100,
+                        alignment: Alignment.center,
                         color: Colors.primaries[index],
+                            child: Text("El color $index"),
                       ),
                     ),
                   ),
