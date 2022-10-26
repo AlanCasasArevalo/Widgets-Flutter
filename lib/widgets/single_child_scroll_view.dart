d6d163cebf236fffd625cf563fea0ac843629654
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-26 19:25:37 +0200

    MySingleChildScrollView

--- lib/widgets/single_child_scroll_view.dart
+++ lib/widgets/single_child_scroll_view.dart
@@ -4,34 +4,49 @@ class MySingleChildScrollView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        bottom: false,
-        child: Column(
-          children: [
-            Text("Hola"),
-            Expanded(
-              child: SizedBox(
-                height: 350,
-                child: SingleChildScrollView(
-                  // Con el parametro reverse (por defecto) hacemos que el scroll empiece al final del listado de elementos (como en whatsapp o telegram)
-                  reverse: false,
-                  child: Column(
-                    children: List.generate(
-                      18,
-                          (index) => Container(
-                        height: 100,
-                        alignment: Alignment.center,
-                        color: Colors.primaries[index],
-                            child: Text("El color $index"),
-                      ),
+        body: SafeArea(
+      bottom: false,
+      child: Column(
+        children: [
+          Container(
+            height: 100,
+            // Este SingleChildScrollView contiene una celda y con el parametro scrollDirection (por defecto con Axis.vertical) se haga scroll horizontal
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(
+                  18,
+                  (index) => Container(
+                    height: 100,
+                    width: 100,
+                    alignment: Alignment.center,
+                    color: Colors.primaries[index],
+                    child: Text("El color $index"),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: SizedBox(
+              height: 350,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: List.generate(
+                    18,
+                    (index) => Container(
+                      height: 100,
+                      alignment: Alignment.center,
+                      color: Colors.primaries[index],
+                      child: Text("El color $index"),
                     ),
                   ),
                 ),
               ),
             ),
-          ],
-        ),
-      )
-    );
+          ),
+        ],
+      ),
+    ));
   }
 }
