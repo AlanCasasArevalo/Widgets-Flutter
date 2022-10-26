9b184ae42881aa6e7601506bd17ed6e7ec26d4fb
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-26 21:29:19 +0200

    MyListView

--- lib/widgets/list_view.dart
+++ lib/widgets/list_view.dart
@@ -5,60 +5,65 @@ class MyListView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: ListView(
-          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-          children: [
-            TextField(),
-            Text("Hola"),
-            Container(
-              height: 100,
-              color: Colors.blue,
-            ),
-            Container(
-              height: 100,
-              color: Colors.red,
-            ),
-            Container(
-              height: 100,
-              color: Colors.blue,
-            ),
-            Text("Hola"),
-            Container(
-              height: 300,
-              color: Colors.red,
-            ),
-            Text("Hola"),
-            Container(
-              height: 300,
-              color: Colors.blue,
-            ),
-            Text("Hola"),
-            Container(
-              height: 300,
-              color: Colors.red,
-            ),
-            Text("Hola"),
-            Container(
-              height: 100,
-              color: Colors.blue,
-            ),
-            Text("Hola"),
-            Container(
-              height: 300,
-              color: Colors.red,
-            ),
-            Text("Hola"),
-            Container(
-              height: 300,
-              color: Colors.blue,
-            ),
-            Text("Hola"),
-            Container(
-              height: 300,
-              color: Colors.red,
-            ),
-            Text("Hola"),
-          ],
+        // El tamaño de la ListView viene dado por el padre que la contenga si usamos un SizedBox con altura de 400
+        // esa sera la altura de la lista de elementos, adecuandolo a dicha altura.
+        child: SizedBox(
+          height: 400,
+          child: ListView(
+            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+            children: [
+              TextField(),
+              Text("Hola"),
+              Container(
+                height: 100,
+                color: Colors.blue,
+              ),
+              Container(
+                height: 100,
+                color: Colors.red,
+              ),
+              Container(
+                height: 100,
+                color: Colors.blue,
+              ),
+              Text("Hola"),
+              Container(
+                height: 300,
+                color: Colors.red,
+              ),
+              Text("Hola"),
+              Container(
+                height: 300,
+                color: Colors.blue,
+              ),
+              Text("Hola"),
+              Container(
+                height: 300,
+                color: Colors.red,
+              ),
+              Text("Hola"),
+              Container(
+                height: 100,
+                color: Colors.blue,
+              ),
+              Text("Hola"),
+              Container(
+                height: 300,
+                color: Colors.red,
+              ),
+              Text("Hola"),
+              Container(
+                height: 300,
+                color: Colors.blue,
+              ),
+              Text("Hola"),
+              Container(
+                height: 300,
+                color: Colors.red,
+              ),
+              Text("Hola"),
+            ],
+          ),
         ),
       ),
     );
