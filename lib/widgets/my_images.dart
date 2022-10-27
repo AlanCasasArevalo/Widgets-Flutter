274becadb184d2de3340e2bdfcd58266b95bcc31
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-27 18:20:18 +0200

    MyImages

--- lib/widgets/my_images.dart
+++ lib/widgets/my_images.dart
@@ -24,14 +24,18 @@ class MyImages extends StatelessWidget {
             ),
             Container(
               color: Colors.grey,
-              // Para cargar una imagen desde internet
               child: Image.network(
                 'https://www.fayerwayer.com/resizer/Khft4HPwSZsHfQCGd4lT_ZXcClA=/800x0/filters:format(jpg):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/metroworldnews/FNOHDGI3YZG4HAPGTYX4LPMZWY.jpg',
-                // Esta parte te permite dejar un loading cargando hasta que la imagen haya terminado la descarga y este lista para ser vista
-                loadingBuilder: (_, __, ___) {
-                  return Center(
-                    child: const CircularProgressIndicator(),
-                  );
+                // Primer valor _ context, segundo valor, la imagen ya descargada, tercer valor el evento de descarga,
+                // una vez terminado queda en null por tanto ya podemos devolver la imagen que ya tenemos descargada
+                loadingBuilder: (_, imageDownloaded, event) {
+                  if (event == null) {
+                    return imageDownloaded;
+                  } else {
+                    return Center(
+                      child: const CircularProgressIndicator(),
+                    );
+                  }
                 },
               ),
             ),
