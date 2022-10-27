6d746a51b54c94625ec54ebf4c723f2547f372c8
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-27 15:04:37 +0200

    MyCustomFonts

--- lib/widgets/custom_font.dart
+++ lib/widgets/custom_font.dart
@@ -11,6 +11,19 @@ class MyCustomFonts extends StatelessWidget {
             Text(
               style: TextStyle(
                 fontSize: 20,
+                fontFamily: 'Nonito',
+                fontWeight: FontWeight.w300,
+                // fontStyle: FontStyle.italic,
+              ),
+                '''
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+           when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          '''
+            ),
+            Text(
+              style: TextStyle(
+                fontSize: 20,
                 fontFamily: 'Titillium',
                 fontWeight: FontWeight.w900,
                 fontStyle: FontStyle.italic,
@@ -19,11 +32,8 @@ class MyCustomFonts extends StatelessWidget {
           Lorem Ipsum is simply dummy text of the printing and typesetting industry.
            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            It has survived not only five centuries, but also the leap into electronic typesetting,
-             remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-              and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           '''
-            )
+            ),
           ],
         ),
       ),
