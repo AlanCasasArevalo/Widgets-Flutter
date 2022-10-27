0cefae730659ced095a83b50688e90fd114b172e
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-27 07:13:43 +0200

    MyListView

--- lib/widgets/list_view.dart
+++ lib/widgets/list_view.dart
@@ -5,65 +5,85 @@ class MyListView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        // El tamaño de la ListView viene dado por el padre que la contenga si usamos un SizedBox con altura de 400
-        // esa sera la altura de la lista de elementos, adecuandolo a dicha altura.
-        child: SizedBox(
-          height: 400,
-          child: ListView(
-            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-            children: [
-              TextField(),
-              Text("Hola"),
-              Container(
-                height: 100,
-                color: Colors.blue,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children:
+                  List.generate(
+                    18,
+                    (index) => Container(
+                      alignment: Alignment.center,
+                      width: 100,
+                      height: 100,
+                      color: Colors.primaries[index],
+                      child: Text("$index"),
+                    ),
+                  )
+                ,
               ),
-              Container(
-                height: 100,
-                color: Colors.red,
+            ),
+            Expanded(
+              child: ListView(
+                keyboardDismissBehavior:
+                    ScrollViewKeyboardDismissBehavior.onDrag,
+                children: [
+                  TextField(),
+                  Text("Hola"),
+                  Container(
+                    height: 100,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    height: 100,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    height: 100,
+                    color: Colors.blue,
+                  ),
+                  Text("Hola"),
+                  Container(
+                    height: 300,
+                    color: Colors.red,
+                  ),
+                  Text("Hola"),
+                  Container(
+                    height: 300,
+                    color: Colors.blue,
+                  ),
+                  Text("Hola"),
+                  Container(
+                    height: 300,
+                    color: Colors.red,
+                  ),
+                  Text("Hola"),
+                  Container(
+                    height: 100,
+                    color: Colors.blue,
+                  ),
+                  Text("Hola"),
+                  Container(
+                    height: 300,
+                    color: Colors.red,
+                  ),
+                  Text("Hola"),
+                  Container(
+                    height: 300,
+                    color: Colors.blue,
+                  ),
+                  Text("Hola"),
+                  Container(
+                    height: 300,
+                    color: Colors.red,
+                  ),
+                  Text("Hola"),
+                ],
               ),
-              Container(
-                height: 100,
-                color: Colors.blue,
-              ),
-              Text("Hola"),
-              Container(
-                height: 300,
-                color: Colors.red,
-              ),
-              Text("Hola"),
-              Container(
-                height: 300,
-                color: Colors.blue,
-              ),
-              Text("Hola"),
-              Container(
-                height: 300,
-                color: Colors.red,
-              ),
-              Text("Hola"),
-              Container(
-                height: 100,
-                color: Colors.blue,
-              ),
-              Text("Hola"),
-              Container(
-                height: 300,
-                color: Colors.red,
-              ),
-              Text("Hola"),
-              Container(
-                height: 300,
-                color: Colors.blue,
-              ),
-              Text("Hola"),
-              Container(
-                height: 300,
-                color: Colors.red,
-              ),
-              Text("Hola"),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
