856cf1fbdd8f2d3e4d9c860b6f4c9564a6c85208
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-27 18:17:28 +0200

    MyImages

--- lib/widgets/my_images.dart
+++ lib/widgets/my_images.dart
@@ -4,20 +4,38 @@ class MyImages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          // Si ponemos al contenedor de la imagen unas dimensiones, las dimensiones definidas en la
-          // Imagen sera ignorada cogiendo solo las del padre
-          height: 200,
-          width: 300,
-          color: Colors.grey,
-          child: Image.asset(
-            'assets/images/wolverine_4.png',
-            height: 600,
-            width: 400,
-            // El parametro cover hace que la imagen se expanda hasta alcanzar el alto y ancho posible definido por nosotros
-            fit: BoxFit.cover,
-          ),
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.all(16),
+          children: [
+            Container(
+              height: 200,
+              width: 300,
+              color: Colors.grey,
+              child: Image.asset(
+                'assets/images/wolverine_4.png',
+                height: 600,
+                width: 400,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16),
+            ),
+            Container(
+              color: Colors.grey,
+              // Para cargar una imagen desde internet
+              child: Image.network(
+                'https://www.fayerwayer.com/resizer/Khft4HPwSZsHfQCGd4lT_ZXcClA=/800x0/filters:format(jpg):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/metroworldnews/FNOHDGI3YZG4HAPGTYX4LPMZWY.jpg',
+                // Esta parte te permite dejar un loading cargando hasta que la imagen haya terminado la descarga y este lista para ser vista
+                loadingBuilder: (_, __, ___) {
+                  return Center(
+                    child: const CircularProgressIndicator(),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
