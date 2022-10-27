db286553207070be2bb8a22a215ada29ce0d9d52
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-27 18:01:40 +0200

    MyImages

--- lib/widgets/my_images.dart
+++ lib/widgets/my_images.dart
@@ -11,7 +11,7 @@ class MyImages extends StatelessWidget {
             'assets/images/wolverine_4.png',
             height: 300,
             width: 400,
-            // El parametro fit permite hacer que la imagen se deforme hasta alcanzar los valores que le pusimos nosotros
+            // El parametro fill permite hacer que la imagen se deforme hasta alcanzar los valores que le pusimos nosotros
             // como alto y ancho
             fit: BoxFit.fill,
           ),
