e5c90333a718e7d02dd2e6ec09426b2d117613a0
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-27 07:58:38 +0200

    MyListView

--- lib/widgets/list_view_builder.dart
+++ lib/widgets/list_view_builder.dart
@@ -5,20 +5,25 @@ class MyListViewBuilder extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: ListView(
-        children: List.generate(
-          1000,
-          (index) {
-            print("🧨 $index");
-            return Container(
-              height: 100,
-              margin: EdgeInsets.all(8),
-              color: Colors.green,
-              child: FlutterLogo(),
-              alignment: Alignment.center,
-            );
-          },
-        ),
+      // Creando el ListView como un builder evitamos problemas de renderimiento, lo que permite crear listas muy muy grandes, con imagenes sonidos videos etc y solo renderiza las 3 celdas siguientes que serian mostradas, por tanto evita que el sistema se sobrecargue
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          print("🧨 $index");
+          return Container(
+            height: 100,
+            margin: EdgeInsets.all(8),
+            color: Colors.green,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FlutterLogo(),
+                Text("$index")
+              ],
+            ),
+            alignment: Alignment.center,
+          );
+        },
+        itemCount: 10000,
       ),
     );
   }
