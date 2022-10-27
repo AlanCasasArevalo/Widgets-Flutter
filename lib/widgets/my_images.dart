9c2db3053cc097e92b2ff694c1636651357decf9
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-27 18:37:09 +0200

    MyImages

--- lib/widgets/my_images.dart
+++ lib/widgets/my_images.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class MyImages extends StatelessWidget {
@@ -39,6 +40,18 @@ class MyImages extends StatelessWidget {
                 },
               ),
             ),
+            Padding(
+              padding: EdgeInsets.all(16),
+            ),
+            Center(
+              // Se instala la dependencia de CachedNetworkImage, esta dependencia necesita que se realice un relanzado de la aplicacion debido a que utiliza codigo nativo para hacer el guardado de dicha imagen en los distintos dispositivos de plataforma, android y iOS
+              child: CachedNetworkImage(
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => Icon(Icons.error_outline),
+                imageUrl:
+                    'https://www.fayerwayer.com/resizer/Khft4HPwSZsHfQCGd4lT_ZXcClA=/800x0/filters:format(jpg):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/metroworldnews/FNOHDGI3YZG4HAPGTYX4LPMZWY.jpg',
+              ),
+            )
           ],
         ),
       ),
