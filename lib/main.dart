57e3550b456da0543c94875ff9916a2bc15826d0
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-28 19:52:43 +0200

    Se instala la dependencia de device_preview, para poder ver distintos tamaños de dispositivos moviles

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:widgets/facebook_ui/facebook_ui.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(DevicePreview(
+    enabled: true,
+    builder: (context) => MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -12,6 +16,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      useInheritedMediaQuery: true,
+      builder: DevicePreview.appBuilder,
+      locale: DevicePreview.locale(context),
       home: MyFacebookUI(),
     );
   }
